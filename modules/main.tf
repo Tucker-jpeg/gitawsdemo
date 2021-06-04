b129c223ee5646df14c66fbfed00edbf35bce8a1
Nick Rondeau <68921974+ronnic1@users.noreply.github.com>
2021-06-03 17:09:36 -0700

    Update main.tf
    updated module main.tf file, variable file no longer needed at this time

--- modules/main.tf
+++ modules/main.tf
@@ -1,14 +1,67 @@
-resource "aws_subnet" "gitawsdemo" {
-    vpc_id     = var.vpc_id
-    cidr_block = var.cidr_block
+#Terraform, Gitlab and AWS
+#1 Create a terraform module template that creates:
+#	1. VPC with CIDR 10.0.0.0/16
+#	2. 2 subnets (public) with CIDR 10.0.1.0/24 and 10.0.2.0/24
+#	3. An autoscaling group with Amazon Linux 2 instance (t3.nano) with a min of 2 instances and max of 3
+#	4. Create a bucket to store your terraform state
+
+#2  Use Gitlab to create a CI/CD pipeline that will test the and deploy the infrastructure through AWS
+
+
+provider "aws" {
+    region = "us-east-1"
 }
 
-resource "aws_instance" "gitawsdemo" {
-    ami           = var.ami
-    instance_type = var.instance_type
-    subnet_id     = aws_subnet.webserver.id
+#1 Create a VPC with CIDR 10.0.0.0/16
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+
 
     tags = {
-        Name = var.name
+        Name = "Main VPC"
     }
 }
+
+#2 Two Public Subnets 10.0.1.0/24 and 10.0.2.0/24
+resource "aws_subnet" "Public1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public-Subnet 1"
+  }
+}
+
+resource "aws_subnet" "Public2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "Public-Subnet 2"
+  }
+}
+#3 Create an autoscaling group with Amazon Linux 2 instance (t3.nano) with a min of 2 instances and max of 3
+
+#4 Create an S3 Bucket to store our terraform state file
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tfstate"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "app-state"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
